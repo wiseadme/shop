c676944fb7054b4dc9de2a1675168667f432a526
wiseadme <wiseadme@gmail.com>
2019-12-05 23:33:59 +0300

    correct

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -1,15 +1,27 @@
 const mongoose = require('mongoose')
 const Category = require('../models/Category')
+const errorHandler = require('../utils/errorHandler')
 
-async function createCategory(req, res){
-  res.status(200).json({
-    ok: true,
-    message: `Вы создали категорию ${req.body.name}`,
-    category: req.body
+async function createCategory(req, res) {
+  const category = new Category({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name
   })
+
+  try {
+    category.save().then(category => {
+      res.status(200).json({
+        ok: true,
+        message: `Вы создали категорию ${req.body.name.toLowerCase()}`,
+        category
+      })
+    })
+  } catch (err) {
+    errorHandler(err)
+  }
 }
 
-async function readCategory(req, res){
+async function readCategory(req, res) {
 
 }
 
